359e4e4f46235a97d2d46f9843a50f1e57079ac4
jitups <jitusurve@gmail.com>
2020-06-19 23:33:58 +0530

    added native storage to save user name

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -4,8 +4,8 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
 import { Tab1PageRoutingModule } from './tab1-routing.module';
+import { NativeStorage } from '@ionic-native/native-storage/ngx';
 
 @NgModule({
   imports: [
@@ -13,8 +13,9 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab1PageRoutingModule
+    Tab1PageRoutingModule,
   ],
-  declarations: [Tab1Page]
+  declarations: [Tab1Page],
+  providers: [NativeStorage]
 })
-export class Tab1PageModule {}
+export class Tab1PageModule { }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,23 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { DatePipe } from '@angular/common';
+import { NativeStorage } from '@ionic-native/native-storage/ngx';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
   public Entry: string;
+  public UserName: string;
+  public IsUserNameSaved: boolean = false;
 
-  constructor(private db: AngularFireDatabase) { }
+
+  constructor(private db: AngularFireDatabase,
+    private nativeStorage: NativeStorage
+  ) {
+
+  }
+
+  ngOnInit() {
+    this.checkSavedName();
+  }
+
+  saveUserName() {
+    this.nativeStorage.setItem("userName", this.UserName).then(value => console.log(value));
+    this.checkSavedName();
+  }
+
+  private checkSavedName() {
+    this.nativeStorage.getItem("userName").then(item => {
+      if (!item) {
+        this.IsUserNameSaved = false;
+      }
+      else {
+        this.IsUserNameSaved = true;
+        this.UserName = item;
+      }
+    });
+  }
 
   addItem() {
     let now = new Date();
     let date = new DatePipe('en-in').transform(now, 'dd MMM yy');
     let time = new DatePipe('en-in').transform(now, 'hh:mm');
+    let author = this.UserName;    
 
-    this.db.list('/Diary').push({ 'Date': date, 'Time': time, 'Details': this.Entry });
+    this.db.list('/Diary').push({ 'Date': date, 'Time': time, 'Details': this.Entry, 'Author': author });
   }
 
 }
